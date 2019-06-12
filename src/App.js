1aaed7a26e6c4c4a78cfdbbe3955b1538c298ad9
Mark Nagel <mark_nagel@hotmail.com>
2019-06-12 18:11:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import axios from 'axios';
 import Login from './components/Login/Login';
 import Budget from './components/Budget/Budget';
 import './App.css';
+import {Provider} from 'react-redux'
+import store from './store'
 
 class App extends Component {
 
   render() {
     return (
+      <Provider store = {store}>
       <HashRouter>
         <Switch>
           <Route path='/' component={Login} exact />
           <Route path='/my-budget' component={Budget} />
         </Switch>
       </HashRouter>
+      </Provider>
     );
   }
 }
